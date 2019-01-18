5f57f9a8bfff304b6f3401a1a7742915e5f05140
NikulinIlya <ilia.nikulin@gmail.com>
2019-01-18 20:22:09 +0300

    feat(controllers): add middleware auth to chat controller

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -9,6 +9,16 @@ use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function chat()
     {
         return view('chat');
