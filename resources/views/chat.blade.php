03a1af3638a397ba852d35369428cac0fa886509
NikulinIlya <ilia.nikulin@gmail.com>
2019-01-18 17:13:51 +0300

    feat(view): make message component

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -8,11 +8,25 @@
     <title>Chat</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+        .list-group {
+            overflow-y: scroll;
+            height: 200px;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <div class="row" id="app">
-            <h1>Chat Room</h1>
+            <div class="offset-4 col-4">
+                <li class="list-group-item active">Chat Room</li>
+                <ul class="list-group">
+                    <message-component v-for="value in chat.message">
+                        @{{ value }}
+                    </message-component>
+                </ul>
+                <input type="text" class="form-control" placeholder="Type your message here..." v-model="message" @keyup.enter="send">
+            </div>
         </div>
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
